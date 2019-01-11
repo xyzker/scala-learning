3c88c9e823b301d49e15af5a750c0b4f3c8b7241
kexi <kexi@meituan.com>
2019-01-11 13:56:43 +0800

    add example

--- src/main/scala/ex/example.scala
+++ src/main/scala/ex/example.scala
@@ -1,16 +1,66 @@
 package ex
 
+import scala.io.Source
+
 /**
   * @author: kexi
   * @date: 2019/1/11
   */
 object example {
   def main(args: Array[String]): Unit = {
+    read(Array("build.sbt"))
+  }
+
+  def array(): Unit ={
     val greetStrings =new Array[String](3)
     greetStrings(0)="Hello"
     greetStrings(1)=","
     greetStrings(2)="world!\n"
     for(i <- 0 to 2)
       print(greetStrings(i))
+  }
+
+  def list(): Unit ={
+    val oneTwo = List(1,2)
+    val threeFour = List(3,4)
+    val oneTwoThreeFour=oneTwo ::: threeFour
+    println (oneTwo + " and " + threeFour + " were not mutated.")
+    println ("Thus, " + oneTwoThreeFour + " is a new list")
+  }
+
+  def list2: Unit ={
+    val oneTowThree = 1 :: 2 ::3 :: Nil
+    println(oneTowThree)
+  }
+
+  def tuples: Unit ={
+    val pair=(99,"Luftballons")
+    println(pair._1)
+    println(pair._2)
+  }
+
+  def set: Unit ={
+    var jetSet = Set ("Boeing","Airbus")
+    jetSet +="Lear"
+    println(jetSet.contains("Cessna"))
+  }
+
+  def map: Unit ={
+    val romanNumeral = Map ( 1 -> "I" , 2 -> "II",
+      3 -> "III", 4 -> "IV", 5 -> "V")
+    println (romanNumeral(4))
+  }
+
+  def printArgs ( args: Array[String]) : Unit ={
+    args.foreach(println)
+  }
+
+  def read(args: Array[String]): Unit ={
+    if (args.length >0 ){
+      for( line <- Source.fromFile(args(0)).getLines())
+        println(line.length + " " + line)
+    }
+    else
+      Console.err.println("Please enter filename")
   }
 }
